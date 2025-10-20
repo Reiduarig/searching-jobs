edaa5fde8e259667dd1b034fe9d2cfa6657c27a5
Reiduarig <giraudier82@gmail.com>
2025-10-20 14:34:37 +0200

    crear card para los jobs

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From €60000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div class="space-x-1">
+            <x-tag>Tag 1</x-tag>
+            <x-tag>Tag 2</x-tag>
+            <x-tag>Tag 3</x-tag>
+        </div>
+
+        <img src="#" alt="imagen" class="rounded-xl">
+    </div>
+</div>
