84da260b7168dde6a7f105c91dc8d91020be40a8
Reiduarig <giraudier82@gmail.com>
2025-10-21 12:32:16 +0200

    crear componente reutilizable tag

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,3 +1,20 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">
+@props([
+    'size' => 'base'
+])
+
+@php
+    $classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300';
+    
+    match ($size) {
+        'base' => $classes .= ' px-5 py-1 text-sm',
+        'small' => $classes .= ' px-2 py-1 text-2xs',
+    }
+    
+@endphp
+
+<a 
+    href="#" 
+    class="{{ $classes }}"
+>
     {{ $slot }}
 </a>
