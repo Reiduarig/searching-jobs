ba1317c16d00c1f3e22aa1e0b623ce30857ab3c8
Reiduarig <giraudier82@gmail.com>
2025-10-21 19:40:23 +0200

    reestructuración de las rutas

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,33 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TagController;
 
 Route::get('/', [JobController::class,'index'])->name('jobs');
+
+Route::get('/search', SearchController::class)->name('search');
+Route::get('/tags/{tag:name}', TagController::class);
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterUserController::class,'create'])->name('register');
+    Route::post('/register', [RegisterUserController::class,'store']);
+    Route::get('/login', [SessionController::class,'create'])->name('login');
+    Route::post('/login', [SessionController::class,'store']);
+});
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/jobs/create', [JobController::class,'create'])->name('jobs.create');
+    Route::post('/jobs', [JobController::class,'store'])->name('jobs.store');
+    Route::delete('/logout', [SessionController::class,'destroy'])->name('logout');
+
+});
+
+
+
+
