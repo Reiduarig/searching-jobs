9833f19a540e724126a97968321a1389f4daa267
Reiduarig <giraudier82@gmail.com>
2025-10-21 19:40:10 +0200

    condicionar en layout la visualizacion dependiiendo de la autenticacion

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,23 +16,38 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
-<body class="bg-black text-white font-sans">
+<body class="bg-black text-white font-sans pb-20">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10 mb-10">
             <div>
-                <a href="#">
+                <a href="{{ route('jobs') }}">
                     <img src="/images/logo.png" alt="Logo">
                 </a>
             </div>
             <div class="space-x-6 font-bold">
-                <a href="#">Trabajos</a>
+                <a href="{{ route('jobs') }}">Trabajos</a>
                 <a href="#">Carreras</a>
                 <a href="#">Salarios</a>
                 <a href="#">Compañias</a>
             </div>
-            <div>
-                <a href="#">Postear</a>
-            </div>
+            @auth
+                <div class="space-x-6 font-bold flex items-center">
+                    <a href="/jobs/create">Postear</a>
+
+                    <form method="POST" action="/logout" >
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Cerrar Sesión</button>
+                    </form>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="{{ route('login') }}">Iniciar Sesión</a>
+                    <a href="{{ route('register') }}">Registrarse</a>
+                </div>
+            @endguest
         </nav>
         <main class="max-w-[986px] mx-auto">
             {{ $slot }}
