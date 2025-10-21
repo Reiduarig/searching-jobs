3f02ad464f6dbcc80d1027ca151e5b585ca59a0b
Reiduarig <giraudier82@gmail.com>
2025-10-21 12:32:01 +0200

    crear otro componente para los card anchos

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-600">Laracasts</a>
+        
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From €60000</p>
+    </div>
+
+    <div class="space-x-1 mt-auto">  
+        <x-tag>Tag 1</x-tag>
+        <x-tag>Tag 2</x-tag>
+        <x-tag>Tag 3</x-tag>
+    </div>
+</x-panel>
