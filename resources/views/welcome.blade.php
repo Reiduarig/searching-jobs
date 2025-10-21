adaf0ac9bccd98b8d76d587873ca6b391c2f1bcb
Reiduarig <giraudier82@gmail.com>
2025-10-21 12:32:45 +0200

    seccion en welcome para la búsqueda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,22 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Encuentra el trabajo de tus sueños</h1>
+            <form action="#" class="mt-6">
+                <input 
+                    type="text" 
+                    placeholder="¿Qué trabajo estás buscando?" 
+                    class="w-full max-w-xl bg-white/5 placeholder-gray-400 rounded-xl px-5 py-3 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:bg-white/10 transition"
+                >
+                <button 
+                    type="submit" 
+                    class="bg-blue-800 hover:bg-blue-900 text-white font-bold rounded-xl px-5 py-3 transition"
+                >
+                    Buscar
+                </button>
+            </form>
+        </section>
+        <section class="pt-10">
             <x-section-heading>Destacados</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
                 <x-job-card />
@@ -28,6 +44,13 @@
 
         <section>
             <x-section-heading>Recientes</x-section-heading>
+
+            <div class="mt-6 space-y-4">
+                <x-job-card-wide />
+                <x-job-card-wide />
+                <x-job-card-wide />
+            </div>
+        
         </section>
     </div>
 </x-layout>
