d7816e5bc6647d842c61ce3614eea6a044e35981
Reiduarig <giraudier82@gmail.com>
2025-10-31 09:57:02 +0100

    nuevos metodos para modelo user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -22,6 +23,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'newsletter',
+        'role',
+        'notification_preferences',
     ];
 
     /**
@@ -44,11 +48,64 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'newsletter' => 'boolean',
+            'notification_preferences' => 'array',
         ];
     }
 
     public function employer(): HasOne
     {
         return $this->hasOne(Employer::class);
+    }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+
+    public function savedJobs(): HasMany
+    {
+        return $this->hasMany(SavedJob::class);
+    }
+
+    public function hasAppliedTo(Job $job): bool
+    {
+        return $this->applications()->where('job_id', $job->id)->exists();
+    }
+
+    public function hasSaved(Job $job): bool
+    {
+        return $this->savedJobs()->where('job_id', $job->id)->exists();
+    }
+
+    public function isEmployer(): bool
+    {
+        return $this->role === 'employer';
+    }
+
+    public function isCandidate(): bool
+    {
+        return $this->role === 'candidate';
+    }
+
+    public function getNotificationPreferences(): array
+    {
+        return $this->notification_preferences ?? [
+            'new_applications' => true,
+            'status_changes' => true,
+            'job_recommendations' => true,
+            'marketing' => false,
+        ];
+    }
+
+    public function shouldReceiveNotification(string $type): bool
+    {
+        $preferences = $this->getNotificationPreferences();
+        return $preferences[$type] ?? false;
+    }
+
+    public function updateNotificationPreferences(array $preferences): void
+    {
+        $this->update(['notification_preferences' => $preferences]);
     }
 }
