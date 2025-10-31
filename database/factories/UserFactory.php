b872a1b86caa4bb413eab5f003b6f7d50c7dc124
Reiduarig <giraudier82@gmail.com>
2025-10-31 09:59:22 +0100

    agregar newsletter al factory de usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'newsletter' => fake()->boolean(30), // 30% chance of subscribing to newsletter
             'remember_token' => Str::random(10),
         ];
     }
